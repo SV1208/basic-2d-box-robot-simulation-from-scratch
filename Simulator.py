124ab193352baed17c47ec9ec5c252128b666566
Shivanshu <shivanshuv999@gmail.com>
2026-02-12 20:08:08 +0530

    feat: separate the loop logic, and implement log facility

--- Simulator.py
+++ Simulator.py
@@ -66,50 +66,49 @@ class MovementSystem(sdl2.ext.Applicator):
 class Simulator2D(Simulator):
     def __init__(self):
         super().__init__()
-
-    
-    def run(self):
         sdl2.ext.init()
         self.window = sdl2.ext.Window("Simulator", size = (800, 600))
         self.window.show()
 
-        world = sdl2.ext.World()
+        self.world = sdl2.ext.World()
 
         movement = MovementSystem(0, 0, 800, 600)
         spriterenderer = SoftwareRenderer(self.window)
 
-        world.add_system(movement)
-        world.add_system(spriterenderer)
+        self.world.add_system(movement)
+        self.world.add_system(spriterenderer)
 
         factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
         robot_body = factory.from_color(WHITE, size=(20, 20))
 
-        r1 = Robot(world, robot_body, 250, 250)
-        # r1.velocity.vx = 1
+        self.r1 = Robot(self.world, robot_body, 250, 250)
+
+    
+    def run(self, delay=0):
+        # self.r1.velocity.vx = 1
 
         self.running = True
-        while self.running:
-            for event in sdl2.ext.get_events():
-                if event.type == sdl2.SDL_QUIT:
-                    self.running = False
-                    break
-                if event.type == sdl2.SDL_KEYDOWN:
-                    if event.key.keysym.sym == sdl2.SDLK_UP:
-                        r1.velocity.vy = -3
-                    elif event.key.keysym.sym == sdl2.SDLK_DOWN:
-                        r1.velocity.vy = 3
-                    if event.key.keysym.sym == sdl2.SDLK_LEFT:
-                        r1.velocity.vx = -3
-                    elif event.key.keysym.sym == sdl2.SDLK_RIGHT:
-                        r1.velocity.vx = 3
-                elif event.type == sdl2.SDL_KEYUP:
-                    if event.key.keysym.sym in (sdl2.SDLK_UP, sdl2.SDLK_DOWN):
-                        r1.velocity.vy = 0
-                    
-                    if event.key.keysym.sym in (sdl2.SDLK_LEFT, sdl2.SDLK_RIGHT):
-                        r1.velocity.vx = 0
-            
-            sdl2.SDL_Delay(10)
-            world.process()
-        sdl2.ext.quit()
-        return 0
+        # while self.running:
+        for event in sdl2.ext.get_events():
+            if event.type == sdl2.SDL_QUIT:
+                sdl2.ext.quit()
+                return 0
+            if event.type == sdl2.SDL_KEYDOWN:
+                if event.key.keysym.sym == sdl2.SDLK_UP:
+                    self.r1.velocity.vy = -3
+                elif event.key.keysym.sym == sdl2.SDLK_DOWN:
+                    self.r1.velocity.vy = 3
+                if event.key.keysym.sym == sdl2.SDLK_LEFT:
+                    self.r1.velocity.vx = -3
+                elif event.key.keysym.sym == sdl2.SDLK_RIGHT:
+                    self.r1.velocity.vx = 3
+            elif event.type == sdl2.SDL_KEYUP:
+                if event.key.keysym.sym in (sdl2.SDLK_UP, sdl2.SDLK_DOWN):
+                    self.r1.velocity.vy = 0
+                
+                if event.key.keysym.sym in (sdl2.SDLK_LEFT, sdl2.SDLK_RIGHT):
+                    self.r1.velocity.vx = 0
+            if delay>0:
+                sdl2.SDL_Delay(10)
+            self.world.process()
+        return 1
--- main.py
+++ main.py
@@ -2,15 +2,30 @@ from Robot import Robot2D
 from Environment import Env2D
 from Simulator import Simulator2D
 
+# define robot such that, we can load sensor, or other processing details in it
 robot = Robot2D()
+
+
+# define environment such t
 env = Env2D()
+
 sim = Simulator2D()
 
 sim.load_env(env)
 sim.load_robot(robot)
 
-sim.run()
 
+running = True
+count = 0
+while running:
+    x, y = sim.r1.sprite.position
+    vx, vy = sim.r1.velocity.vx, sim.r1.velocity.vy
+    if count>100:
+        count = 0
+        print("\nPostion:",x,y)
+        print("Velocity:",vx, vy )
+    count+=0.001
 
+    running = sim.run(delay=10) # it should run only one time step, and not the whole loop, so that loop control is over us
 
 
